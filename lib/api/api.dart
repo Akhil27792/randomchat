f2ae58c7d35270296b0c6183127cd1bc3f0c3453
Krishna <krai100200@gmail.com>
2024-03-08 19:29:37 +0530

    day24 comleted

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,21 +1,65 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:random_chat/models/chat_user.dart';
 
-class API{
+class API {
   //for authentication
-  static FirebaseAuth auth=FirebaseAuth.instance;
- //for accessing cloud firestore database
- static FirebaseFirestore firestore = FirebaseFirestore.instance;
- static User get user=>auth.currentUser!;
- //for checking if user exists or not?
- static Future<bool> userExist() async{
-  return (await firestore.collection('users').doc(auth.currentUser?.uid).get()).exists;
- }
-   //for creating a new user
-static Future<void> createUser()async{
-  final time=DateTime.now().millisecondsSinceEpoch.toString();
-   final chatUser= ChatUser(image: user.photoURL.toString(), about: 'Hi, I am using Random Chat', name: user.displayName.toString(), createdAt:time , isOnline: false, id: user.uid, lastActive: time, pushToken: '', email: user.email.toString());
-   return await firestore.collection('users').doc(user.uid).set(chatUser.toJson());
-}
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  //for accessing cloud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static User get user => auth.currentUser!;
+  //for storing self info
+  static late ChatUser me;
+  //for checking if user exists or not?
+  static Future<bool> userExist() async {
+    return (await firestore
+            .collection('users')
+            .doc(auth.currentUser?.uid)
+            .get())
+        .exists;
+  }
+
+  static Future<void> getSelfInfo() async {
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser?.uid)
+        .get()
+        .then((user) {
+      if (user.exists) {
+        me = ChatUser.fromJson(user.data() as Map<String, dynamic>);
+        log('My Data: ${user.data()}' as num);
+      } else {
+        createUser().then((value) => getSelfInfo());
+      }
+    });
+  }
+
+  //for creating a new user
+  static Future<void> createUser() async {
+    final time = DateTime.now().millisecondsSinceEpoch.toString();
+    final chatUser = ChatUser(
+        image: user.photoURL.toString(),
+        about: 'Hi, I am using Random Chat',
+        name: user.displayName.toString(),
+        createdAt: time,
+        isOnline: false,
+        id: user.uid,
+        lastActive: time,
+        pushToken: '',
+        email: user.email.toString());
+    return await firestore
+        .collection('users')
+        .doc(user.uid)
+        .set(chatUser.toJson());
+  }
+
+//for getting all user from firebase database
+  static Stream<QuerySnapshot<Map<String, dynamic>>> getAllUser() {
+    return firestore
+        .collection('users')
+        .where('id', isNotEqualTo: user.uid)
+        .snapshots();
+  }
 }
--- lib/screens/auth/home_screen.dart
+++ lib/screens/auth/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:random_chat/models/chat_user.dart';
@@ -15,7 +14,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late  List<ChatUser> list = [];
+  late List<ChatUser> list = [];
+  @override
+  void initState() {
+    super.initState();
+    API.getSelfInfo(); // Get self info to display on the profile button
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,45 +33,55 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {},
             icon: const Icon(Icons.search),
           ),
-          IconButton(onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfileScreen(user: list[0],)));
-          }, icon: const Icon(Icons.more_vert))
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProfileScreen(
+                              user: API.me,
+                            )));
+              },
+              icon: const Icon(Icons.more_vert))
         ],
       ),
       //floating button to add new user
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 8.0),
         child: FloatingActionButton(
-          onPressed: ()  {
-          },
+          onPressed: () {},
           child: const Icon(Icons.add_comment_rounded),
         ),
       ),
       body: StreamBuilder(
-        stream: API.firestore.collection('users').snapshots(),
+        stream: API.getAllUser(),
         builder: (context, snapshot) {
-          switch(snapshot.connectionState){
+          switch (snapshot.connectionState) {
             case ConnectionState.waiting:
             case ConnectionState.none:
             case ConnectionState.active:
             case ConnectionState.done:
-             final data = snapshot.data?.docs;
-             list =data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
+              final data = snapshot.data?.docs;
+              list =
+                  data?.map((e) => ChatUser.fromJson(e.data())).toList() ?? [];
           }
-          if(list.isNotEmpty){
+          if (list.isNotEmpty) {
             return ListView.builder(
-              itemCount: list.length,
-              padding: EdgeInsets.only(top: mq.height * .01),
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                // return const ChatUserCard();
-                return ChatUserCard(user: list[index],);
-              });
-          }
-          else{
+                itemCount: list.length,
+                padding: EdgeInsets.only(top: mq.height * .01),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  // return const ChatUserCard();
+                  return ChatUserCard(
+                    user: list[index],
+                  );
+                });
+          } else {
             return const Center(
-              child:Text("No Friends Found",style: TextStyle(fontSize: 20),)
-            );
+                child: Text(
+              "No Friends Found",
+              style: TextStyle(fontSize: 20),
+            ));
           }
         },
       ),
